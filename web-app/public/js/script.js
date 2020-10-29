06405b427c388013dd2372f2a17b157f1f71321a
Horea Porutiu <horeaporutiu@gmail.com>
2020-10-28 19:19:30 -0700

    add completed at, files

--- web-app/public/js/script.js
+++ web-app/public/js/script.js
@@ -35,44 +35,44 @@ form.addEventListener('submit', async (e) => {
   let jsonBody = JSON.parse(body)
   console.log('jsonBody')
   console.log(jsonBody)
-  let inputData = jsonBody.entity.decision_optimization.input_data;
+  let firstFile = jsonBody.entity.decision_optimization.input_data[0];
+  let secondFile = jsonBody.entity.decision_optimization.input_data[1];
+  console.log('firstFile')
+  console.log(firstFile)
+  console.log('secondFile')
+  console.log(secondFile)
 
-  // for (var z = 0; z < inputData.length; z++) {
-
-  //   for (let  i = 0; i < values.length; i++) {
-      
-  //     for (j = 0; j < fields.length; j++) {
-
-  //       solution.innerHTML += '<span>' + '<b>' + fields[j] + ': </b>' + values[i][j] + '</span>'
-  //       solution.innerHTML += '<br>'
-  //     }
-
-  //     solution.innerHTML += '<br>'
-  //   }
-
-  // }
-console.log('inputData')
-console.log(inputData)
 let createdAt = jsonBody.metadata.created_at
-console.log('createdAt')
-console.log(createdAt)
-// let jsonBody = JSON.parse(body)
-// let parsedJSON = await res.json();
-// console.log(body)
-console.log(jsonBody.metadata)
 
-// for (let i = 0; i < values.length; i++) {
-//   for (j = 0; j < fields.length; j++) {
-//     solution.innerHTML += '<span>' + '<b>' + fields[j] + ': </b>' + values[i][j] + '</span>' 
-//     solution.innerHTML += '<br>'
-//   }
-//   solution.innerHTML += '<br>'
-// }
 
 firstSpinner.hidden = true;
 output.innerHTML += '<p id = "jobText" >Job created at: </p>' + '<span>' + createdAt + '</span>';
 output.innerHTML += '<br>'
-output.innerHTML += '<p id = "inputDataText" >Input data from files: </p>' + JSON.stringify(inputData, undefined, 4);
+// output.innerHTML += '<p id = "inputDataText" >Input data from files: </p>' + JSON.stringify(inputData, undefined, 4);
+output.innerHTML += '<b><p id = "fileName1" > Filename: </p></b>'  + '<span>' + firstFile.id + '</span><br>' 
+
+output.innerHTML += '<p id = "inputDataText" >Input data from ' + firstFile.id+ ': </p><br>'
+
+for (let i = 0; i < firstFile.values.length; i++) {
+  for (j = 0; j < firstFile.fields.length; j++) {
+    output.innerHTML += '<span>' + '<b>' + firstFile.fields[j] + ': </b>' + firstFile.values[i][j] + '</span>' 
+    output.innerHTML += '<br>'
+  }
+  output.innerHTML += '<br>'
+}
+
+
+output.innerHTML += '<b><p id = "fileName2" > Filename: </p></b>'  + '<span>' + secondFile.id + '</span><br>' 
+
+output.innerHTML += '<p id = "inputDataText" >Input data from ' + secondFile.id + ': </p><br>'
+
+for (let i = 0; i < secondFile.values.length; i++) {
+  for (j = 0; j < secondFile.fields.length; j++) {
+    output.innerHTML += '<span>' + '<b>' + secondFile.fields[j] + ': </b>' + secondFile.values[i][j] + '</span>' 
+    output.innerHTML += '<br>'
+  }
+  output.innerHTML += '<br>'
+}
 solutionUpdate.hidden = false;
 
 })
