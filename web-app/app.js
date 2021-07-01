9d3e2fb8599f384abf5fa9633afc98f160f0de9b
Mark-Sturdevant <Mark.Sturdevant@ibm.com>
2021-07-01 15:18:49 -0700

    Use eslint/prettier and jshint to format the code

--- web-app/app.js
+++ web-app/app.js
@@ -1,16 +1,16 @@
 'use strict';
 
-const express = require('express')
+const express = require('express');
 const app = express();
 const axios = require('axios');
-var bodyParser = require("body-parser");
+const bodyParser = require('body-parser');
 const fileupload = require('express-fileupload');
-const defaultData = require('./data.js')
+const defaultData = require('./data.js');
 require('dotenv').config();
-var cors = require('cors')
+const cors = require('cors');
 app.use(cors());
 
-//tells the application to use body-parser as middleware so it can handle post requests
+// tells the application to use body-parser as middleware so it can handle post requests
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(fileupload());
@@ -21,104 +21,109 @@ const apiVersion = '2021-06-16';
 let latestTag;
 const postURL = `https://us-south.ml.cloud.ibm.com/ml/v4/deployment_jobs?version=${apiVersion}`;
 
-//create a Watson Machine Learning job to solve a decision optimization problem using input files
+// create a Watson Machine Learning job to solve a decision optimization problem using input files
 app.post('/send', async function (req, res) {
-
-  //parse the inputted files from the UI
+  // parse the inputted files from the UI
   let json = req.files[0].data.toString();
   let plantFile = req.files[1].data.toString();
 
-  //take away special characters to make files easier to parse
+  // take away special characters to make files easier to parse
   json = json.replace(/(?:\r|\r|)/g, '');
   plantFile = plantFile.replace(/(?:\r|\r|)/g, '');
 
-  //split files on new line
-  var lines = json.split("\n");
-  var lines2 = plantFile.split("\n");
+  // split files on new line
+  const lines = json.split('\n');
+  const lines2 = plantFile.split('\n');
 
-  var result = [], result2 = [];
+  const result = [];
+  const result2 = [];
 
-  //set the first split as the headers
-  var headersDemand = lines[0].split(",");
-  var headersPlants = lines2[0].split(",");
+  // set the first split as the headers
+  const headersDemand = lines[0].split(',');
+  const headersPlants = lines2[0].split(',');
 
-  //split csv file by comma, then push into array. 
-  for (var i = 1; i < lines.length - 1; i++) {
-    var currentline = lines[i].split(",");
-    result.push(currentline);
+  // split csv file by comma, then push into array.
+  for (let i = 1; i < lines.length - 1; i++) {
+    result.push(lines[i].split(','));
   }
-  for (var i = 1; i < lines2.length - 1; i++) {
-    var currentline = lines2[i].split(",");
-    result2.push(currentline);
+  for (let i = 1; i < lines2.length - 1; i++) {
+    result2.push(lines2[i].split(','));
   }
 
-  //set customerDemand object
-  let plantsObj = {
+  // set customerDemand object
+  const plantsObj = {
     id: req.files[1].name,
     fields: headersPlants,
-    values: result2
+    values: result2,
   };
 
-  //set plants object
-  let customerDemandsObj = {
+  // set plants object
+  const customerDemandsObj = {
     id: req.files[0].name,
     fields: headersDemand,
-    values: result
+    values: result,
   };
 
-  //add tag before we create a new job to solve a Decision optimization problem
-  latestTag = Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 5);
+  // add tag before we create a new job to solve a Decision optimization problem
+  latestTag = Math.random()
+    .toString(36)
+    .replace(/[^a-z]+/g, '')
+    .substr(0, 5);
   defaultData.requestBody.tags = [latestTag];
 
-  //set the parsed data from the input files as part of our request body
-  defaultData.requestBody.decision_optimization.input_data = [ customerDemandsObj, plantsObj ];
+  // set the parsed data from the input files as part of our request body
+  /* eslint-disable-next-line camelcase */
+  defaultData.requestBody.decision_optimization.input_data = [customerDemandsObj, plantsObj];
 
-  console.log("SEND UPLOADED DATA");
+  console.log('SEND UPLOADED DATA');
 
-  let response = await axios.post(postURL, defaultData.requestBody,
-    {
-      headers: {
-        'Authorization': process.env.TOKEN,
-        'Content-Type': 'application/json',
-      }
-    });
+  const response = await axios.post(postURL, defaultData.requestBody, {
+    headers: {
+      Authorization: process.env.TOKEN,
+      'Content-Type': 'application/json',
+    },
+  });
   res.send(JSON.stringify(response.data));
 });
 
-//create a Watson Machine Learning job to solve a decision optimization problem using default data from the ./data.js file
+// create a Watson Machine Learning job to solve a decision optimization problem using default data from the ./data.js file
 app.post('/sendDefault', async function (req, res) {
-
-
-  //add tag before we create a new job to solve a Decision optimization problem
-  latestTag = Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 5);
+  // add tag before we create a new job to solve a Decision optimization problem
+  latestTag = Math.random()
+    .toString(36)
+    .replace(/[^a-z]+/g, '')
+    .substr(0, 5);
   defaultData.data.tags = [latestTag];
 
-  console.log("SEND DEFAULT DATA");
+  console.log('SEND DEFAULT DATA');
 
-  let response = await axios.post(postURL, defaultData.data,
-    {
-      headers: {
-        'Authorization': process.env.TOKEN,
-        'Content-Type': 'application/json'
-      }
-    })
+  const response = await axios.post(postURL, defaultData.data, {
+    headers: {
+      Authorization: process.env.TOKEN,
+      'Content-Type': 'application/json',
+    },
+  });
   res.send(JSON.stringify(response.data));
 });
 
-//get Watson Machine Learning solution by querying for our job, with the tag we created earlier
+// get Watson Machine Learning solution by querying for our job, with the tag we created earlier
 app.get('/decisionSolution', async function (req, res) {
+  const getURL =
+    'https://us-south.ml.cloud.ibm.com/ml/v4/deployment_jobs?space_id=' +
+    process.env.SPACE_ID +
+    '&tag.value=' +
+    latestTag +
+    '&state=completed&deployment_id=' +
+    process.env.DEPLOYMENT_ID +
+    '&version=' +
+    apiVersion;
 
-  const getURL = 'https://us-south.ml.cloud.ibm.com/ml/v4/deployment_jobs?space_id=' + process.env.SPACE_ID +
-    '&tag.value=' + latestTag + '&state=completed&deployment_id=' + process.env.DEPLOYMENT_ID +
-    '&version=' + apiVersion;
-
-  let response = await axios.get(getURL,
-    {
-      headers: {
-        'Authorization': process.env.TOKEN,
-        'Content-Type': 'application/json'
-      }
-    })
+  const response = await axios.get(getURL, {
+    headers: {
+      Authorization: process.env.TOKEN,
+      'Content-Type': 'application/json',
+    },
+  });
   console.log(JSON.stringify(response.data));
   res.send(JSON.stringify(response.data));
 });
