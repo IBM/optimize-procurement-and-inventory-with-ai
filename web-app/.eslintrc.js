6f96f6785cf9632d320ef7264f5384a0d19e48a8
Mark-Sturdevant <Mark.Sturdevant@ibm.com>
2022-01-19 11:22:13 -0800

    Dependency updates and lint fix

--- web-app/.eslintrc.js
+++ web-app/.eslintrc.js
@@ -1,27 +1,19 @@
 module.exports = {
-  "env": {
-    "node": true,
-    "mocha": true,
+  env: {
+    node: true,
+    mocha: true,
   },
-  "plugins": [
-    "node",
-    "prettier",
-  ],
-  "extends": [
-    "eslint:recommended",
-    "google",
-    "plugin:node/recommended",
-    "prettier",
-  ],
-  "ignorePatterns": ["public", "node_modules"],
-  "rules": {
-    "no-console": 0,
-    "no-process-exit": 0,
-    "node/no-unpublished-require": ["error", {"allowModules": ["chai", "sinon", "sinon-test"]}],
-    "prettier/prettier": ["error", {"singleQuote": true, "printWidth": 160}],
-    "prefer-const": "error",
-    "prefer-rest-params": "off",
-    "valid-jsdoc": "off",
-    "camelcase": ["error", {"allow": ["response_type", "return_context", "user_defined", "webhook_error", "damage_description"]}],
-  }
+  plugins: ['node', 'prettier'],
+  extends: ['eslint:recommended', 'google', 'plugin:node/recommended', 'prettier'],
+  ignorePatterns: ['public', 'node_modules'],
+  rules: {
+    'no-console': 0,
+    'no-process-exit': 0,
+    'node/no-unpublished-require': ['error', { allowModules: ['chai', 'sinon', 'sinon-test'] }],
+    'prettier/prettier': ['error', { singleQuote: true, printWidth: 160 }],
+    'prefer-const': 'error',
+    'prefer-rest-params': 'off',
+    'valid-jsdoc': 'off',
+    camelcase: ['error', { allow: ['response_type', 'return_context', 'user_defined', 'webhook_error', 'damage_description'] }],
+  },
 };
